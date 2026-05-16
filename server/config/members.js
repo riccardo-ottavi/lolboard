046960dcc427de1bf0230bb557e38509b8849305
riccardo-ottavi <ottavi.ps@libero.it>
2026-05-16 13:26:02 +0200

    server init

---
+++ server/config/members.js
@@ -0,0 +1,7 @@
+const members = {
+  "1162708065389379674": "MaryahCarry#EUW",
+  "905209322894655589": "DarkBull97#EUW",
+  "252690405605376000": "Lippocalippo#LIPPO"  
+};
+
+module.exports = { members }
