2d64c850924b594332f1c18cb70883c92d6a5da1
riccardo-ottavi <ottavi.ps@libero.it>
2026-07-10 12:29:16 +0200

    update: footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,15 @@
 export default function footer(){
     return (
         <footer>
+            <div className="project-infos">
+                <p>Applicazione web sviluppata per raccogliere e visualizzare le statistiche degli account League of Legends dei membri di un server Discord. Il progetto permette di monitorare dati di gioco, confrontare le prestazioni dei partecipanti e offrire una panoramica intuitiva dell'attività della community. Durante lo sviluppo ho approfondito l'integrazione con API esterne, la gestione dei dati e la realizzazione di interfacce web.</p>
+            </div>
             <div className="social">
                 <ul>
                     <li><a href="#">Linkedin</a></li>
                     <li><a href="#">Github</a></li>
                     <li><a href="#">Mail</a></li>
                 </ul>
-            </div>
-            <div className="project-infos">
-                <p>Qua ci sarà una spiegazione del progetto</p>
             </div>
         </footer>
     )
