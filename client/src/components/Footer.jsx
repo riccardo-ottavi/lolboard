b6f77a65a863d2cd8dd128df0a26eaa488c890ed
riccardo-ottavi <ottavi.ps@libero.it>
2026-07-13 11:04:05 +0200

    link social (linkedin + github)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,13 +2,12 @@ export default function footer(){
     return (
         <footer>
             <div className="project-infos">
-                <p>Applicazione web sviluppata per raccogliere e visualizzare le statistiche degli account League of Legends dei membri di un server Discord. Il progetto permette di monitorare dati di gioco, confrontare le prestazioni dei partecipanti e offrire una panoramica intuitiva dell'attività della community. Durante lo sviluppo ho approfondito l'integrazione con API esterne, la gestione dei dati e la realizzazione di interfacce web.</p>
+                <p>Raccogli e visualizza le statistiche degli account League of Legends dei membri di un server Discord. Monitora dati di gioco, confronta le prestazioni dei partecipanti in una panoramica intuitiva dell'attività della community.</p>
             </div>
             <div className="social">
                 <ul>
-                    <li><a href="#">Linkedin</a></li>
-                    <li><a href="#">Github</a></li>
-                    <li><a href="#">Mail</a></li>
+                    <li><a href="https://www.linkedin.com/in/riccardo-ottavi-b542533b8/">Linkedin</a></li>
+                    <li><a href="https://github.com/riccardo-ottavi">Github</a></li>
                 </ul>
             </div>
         </footer>
